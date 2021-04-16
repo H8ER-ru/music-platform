4308b0afb935f98efefb2813f85ccf39aef75a94
Nikita <nik.veras2001@gmail.com>
2021-04-16 21:18:01 +0300

    start doing client

---
+++ server/src/app.module.ts
@@ -0,0 +1,17 @@
+import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from './file/file.module'
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from "path";
+
+
+@Module({
+    imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+        MongooseModule.forRoot(process.env.DATABASE_LINK || 'mongodb+srv://nikVeras:Bender2282@cluster0.pyh3q.mongodb.net/music-app?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule
+    ]
+})
+export class AppModule {}
